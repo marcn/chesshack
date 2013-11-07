bd00f82c1b5fa8658199050a3f8684a84d98d7e2
Greg Zavitz <gzavitz@pandora.com>
2013-11-07 12:57:57 -0800

    adaptive threshold

--- board_processing.py
+++ board_processing.py
@@ -1,34 +1,25 @@
 import cv2, numpy as np, math, random, time
 from utils import ImgOut
+from scipy.cluster.vq import kmeans
 
 img_out = ImgOut()
 
 class ChessCV():
 
 	def __init__(self, file_name, scale=0.25):
-		t = int(round(time.time() * 1000))
 		self.image = cv2.imread(file_name)
-		print "read img", (int(round(time.time() * 1000)) - t)
 		self.scale = scale
 		self.dimensions = (len(self.image[0]), len(self.image))
 
 		# find corners of board
-		t = int(round(time.time() * 1000))
-		dst_img = self.grayscale(self.image)
-		dst_img = self.resize(dst_img)
-		# dst_img = self.quantize(dst_img)
-		dst_img = self.threshold(dst_img)
+		dst_img = img_out.save(self.grayscale(self.image))
+		dst_img = img_out.save(self.resize(dst_img))
+		dst_img = img_out.save(self.quantize(dst_img))
+		dst_img = img_out.save(self.threshold(dst_img))
 		(tl, tr, br, bl) = self.find_corners(dst_img)
-		print "find corners", (int(round(time.time() * 1000)) - t)
 
 		# fix perspective
-		t = int(round(time.time() * 1000))
-		dst_img = self.warp_perspective(self.image, tl, tr, br, bl)
-		print "wrap perspective", (int(round(time.time() * 1000)) - t)
-
-		t = int(round(time.time() * 1000))
-		img_out.save(dst_img)
-		print "save img", (int(round(time.time() * 1000)) - t)
+		dst_img = img_out.save(self.warp_perspective(self.image, tl, tr, br, bl))
 
 	def resize(self, img):
 		new_dimensions = (int(self.dimensions[0] * self.scale), int(self.dimensions[1] * self.scale))
@@ -42,21 +33,32 @@ class ChessCV():
 		Z = np.float32(Z)
 
 		criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-		K = 3
-		ret, label, center = cv2.kmeans(Z, K, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+		K = 40
+		ret, label, center = cv2.kmeans(Z, K, criteria, 10, cv2.KMEANS_PP_CENTERS)
 
 		dst = np.uint8(center)
 		dst = dst[label.flatten()]
 		return dst.reshape((img.shape))
 
+	def contrast(self, img):
+		phi = 1
+		theta = 1
+		intensity = 255
+		dst = (255 / 1) * (img / (intensity / theta)) ** 0.5
+		dst = np.array(dst, dtype=np.uint8)
+		return dst
+
 	def threshold(self, img):
-		return cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)[1]
+		return cv2.adaptiveThreshold(img, 127, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+		#return cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)[1]
 
 	def find_corners(self, img):
-		contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+		contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 		contour_areas = [(cv2.contourArea(c), c) for c in contours]
 		contours_sorted = sorted(contour_areas, key=lambda c2: c2[0], reverse=True)
+
+		print "total contours", len(contour_areas)
 
 		(tl, br), (tr, bl) = self.bounding_box(contours_sorted[0][1])
 
@@ -106,12 +108,6 @@ class ChessCV():
 				br = (br[0], y)
 		return ((tl, br), (tr, bl))
 
-for x in np.arange(0.25, 1.25, 0.25):
-	try:
-		t = int(round(time.time() * 1000))
-		ChessCV('board-pictures/board3.jpg', x)
-		print "scale %d%%" % int(x * 100), int(round(time.time() * 1000)) - t
-	except:
-		print x, 'failed'
-		raise
-	print
+t = int(round(time.time() * 1000))
+ChessCV('board-pictures/board2.jpg')
+print "scale %d%%" % int(0.25 * 100), int(round(time.time() * 1000)) - t

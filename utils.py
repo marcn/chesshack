de8020205e0b7c104be7019722b4b2209b8e404b
Greg Zavitz <gzavitz@pandora.com>
2013-11-08 13:05:20 -0800

    forgot utils

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import cv2, numpy as np, shlex, subprocess
 
-def next_frame():
-	args = shlex.split("raspistill -w 480 -h 480 -t 0 -ev 6 -n -o -")
+def _get_frame(width=480, height=480):
+	args = shlex.split("raspistill -w %d -h %d -t 0 -ev 6 -n -o -" % (width, height))
 	p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = p.communicate()
 	nparr = np.frombuffer(out, dtype='uint8')
 	return cv2.imdecode(nparr, 1)
+
+def next_frame():
+	return _get_frame()
+
+def comparison_frames():
+	return [_get_frame(50, 50), _get_frame(50, 50)]
 
 class ImgOut():
 	counter = 1
